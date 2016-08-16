461990d3fc9380dff95aab198f460024d20d0a46
flav <flavien.dibello@in-tact.fr>
2016-08-16 12:28:22 +0200

    move viewmodels folder

---
+++ XamTest/ViewModels/PersonViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace XamTest
+{
+	public class PersonViewModel : INotifyPropertyChanged
+	{
+
+		List<Person> _list;
+		string currentText;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public PersonViewModel(List<Person> list)
+		{
+			_list = list;
+		}
+
+		public List<Person> PersonList
+		{
+			set
+			{
+				if (_list != value)
+				{
+					_list = value;
+
+					if (PropertyChanged != null)
+					{
+						PropertyChanged(this,
+							new PropertyChangedEventArgs("PersonList"));
+					}
+				}
+			}
+			get
+			{
+				return _list;
+			}
+		}
+
+		public string CurrentText
+		{
+			set
+			{
+				if (currentText != value)
+				{
+					currentText = value;
+
+					if (PropertyChanged != null)
+					{
+						PropertyChanged(this,
+							new PropertyChangedEventArgs("CurrentText"));
+					}
+				}
+			}
+			get
+			{
+				return currentText;
+			}
+		}
+
+	}
+}
+
